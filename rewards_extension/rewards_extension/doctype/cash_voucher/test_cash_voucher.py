898973f0f8888ebfbb7be8f4fe5eb63b23892a9d
vinodkumarkolli <vinodkumarkolli@gmail.com>
2025-07-18 17:21:48 +0000

    Epic: Designed Basic Structure

---
+++ rewards_extension/rewards_extension/doctype/cash_voucher/test_cash_voucher.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Vinod Kumar K and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCashVoucher(FrappeTestCase):
+	pass
