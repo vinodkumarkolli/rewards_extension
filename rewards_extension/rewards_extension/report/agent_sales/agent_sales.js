9f2c6279977a5e2f066a20fb62536a07e9f3acfb
vinodkumarkolli <vinodkumarkolli@gmail.com>
2025-08-30 07:04:52 +0000

    chore: added agent sales report structure

---
+++ rewards_extension/rewards_extension/report/agent_sales/agent_sales.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2025, Vinod Kumar K and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Agent Sales"] = {
+	filters: [
+		// {
+		// 	"fieldname": "my_filter",
+		// 	"label": __("My Filter"),
+		// 	"fieldtype": "Data",
+		// 	"reqd": 1,
+		// },
+	],
+};
